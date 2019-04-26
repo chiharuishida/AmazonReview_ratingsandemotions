f2b77bfb7ed6f1f241110c3b56717424687e6217
chiharuishida <49986538+chiharuishida@users.noreply.github.com>
2019-04-26 13:37:23 -0500

    Add files via upload

---
+++ Amazon_Alexa_Sentiment_emotion_analysis_git.R
@@ -0,0 +1,24 @@
+
+install.packages("syuzhet") 
+install.packages("lubridate") 
+install.packages("ggplot2")
+install.packages("scales")
+install.packages("reshape2") 
+install.packages("dplyr") 
+
+
+library(syuzhet)
+library(lubridate)
+library(ggplot2)
+library(scales)
+library(reshape2)
+library(dplyr)
+
+data <- read.csv("https://github.com/chiharuishida/AmazonReview_ratingsandemotions/blob/master/amazon_alexa_textonly.csv")
+
+text=readLines("amazon_alexa_textonly.csv")
+
+sent.text<-get_nrc_sentiment(text)
+head(sent.text)
+sent.text
+write.csv(sent.text, "amazon_alexa_emotions.csv")
